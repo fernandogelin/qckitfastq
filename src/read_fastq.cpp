8ce65e12e761ce75f79ad81014325b3bf5780c2a
ashokrags <ashok.ragavendran@gmail.com>
2018-06-05 17:33:32 -0400

    Cleared out map and replaced with static cast

--- src/read_fastq.cpp
+++ src/read_fastq.cpp
@@ -26,101 +26,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 void process_fastq (std::string infile, std::string out_prefix, int buffer_size)
   {
-  std::map<char, int> ascii_map;
-  ascii_map['!'] = 0;
-  ascii_map['"'] = 1;
-  ascii_map['#'] = 2;
-  ascii_map['$'] = 3;
-  ascii_map['%'] = 4;
-  ascii_map['&'] = 5;
-  ascii_map['\''] = 6;
-  ascii_map['('] = 7;
-  ascii_map[')'] = 8;
-  ascii_map['*'] = 9;
-  ascii_map['+'] = 10;
-  ascii_map[','] = 11;
-  ascii_map['-'] = 12;
-  ascii_map['.'] = 13;
-  ascii_map['/'] = 14;
-  ascii_map['0'] = 15;
-  ascii_map['1'] = 16;
-  ascii_map['2'] = 17;
-  ascii_map['3'] = 18;
-  ascii_map['4'] = 19;
-  ascii_map['5'] = 20;
-  ascii_map['6'] = 21;
-  ascii_map['7'] = 22;
-  ascii_map['8'] = 23;
-  ascii_map['9'] = 24;
-  ascii_map[':'] = 25;
-  ascii_map[';'] = 26;
-  ascii_map['<'] = 27;
-  ascii_map['='] = 28;
-  ascii_map['>'] = 29;
-  ascii_map['?'] = 30;
-  ascii_map['@'] = 31;
-  ascii_map['A'] = 32;
-  ascii_map['B'] = 33;
-  ascii_map['C'] = 34;
-  ascii_map['D'] = 35;
-  ascii_map['E'] = 36;
-  ascii_map['F'] = 37;
-  ascii_map['G'] = 38;
-  ascii_map['H'] = 39;
-  ascii_map['I'] = 40;
-  ascii_map['J'] = 41;
-  ascii_map['K'] = 42;
-  ascii_map['L'] = 43;
-  ascii_map['M'] = 44;
-  ascii_map['N'] = 45;
-  ascii_map['O'] = 46;
-  ascii_map['P'] = 47;
-  ascii_map['Q'] = 48;
-  ascii_map['R'] = 49;
-  ascii_map['S'] = 50;
-  ascii_map['T'] = 51;
-  ascii_map['U'] = 52;
-  ascii_map['V'] = 53;
-  ascii_map['W'] = 54;
-  ascii_map['X'] = 55;
-  ascii_map['Y'] = 56;
-  ascii_map['Z'] = 57;
-  ascii_map['['] = 58;
-  ascii_map['\\'] = 59;
-  ascii_map[']'] = 60;
-  ascii_map['^'] = 61;
-  ascii_map['_'] = 62;
-  ascii_map['`'] = 63;
-  ascii_map['a'] = 64;
-  ascii_map['b'] = 65;
-  ascii_map['c'] = 66;
-  ascii_map['d'] = 67;
-  ascii_map['e'] = 68;
-  ascii_map['f'] = 69;
-  ascii_map['g'] = 70;
-  ascii_map['h'] = 71;
-  ascii_map['i'] = 72;
-  ascii_map['j'] = 73;
-  ascii_map['k'] = 74;
-  ascii_map['l'] = 75;
-  ascii_map['m'] = 76;
-  ascii_map['n'] = 77;
-  ascii_map['o'] = 78;
-  ascii_map['p'] = 79;
-  ascii_map['q'] = 80;
-  ascii_map['r'] = 81;
-  ascii_map['s'] = 82;
-  ascii_map['t'] = 83;
-  ascii_map['u'] = 84;
-  ascii_map['v'] = 85;
-  ascii_map['w'] = 86;
-  ascii_map['x'] = 87;
-  ascii_map['y'] = 88;
-  ascii_map['z'] = 89;
-  ascii_map['{'] = 90;
-  ascii_map['|'] = 91;
-  ascii_map['}'] = 92;
-  ascii_map['~'] = 93;
+  
 
   std::map<std::string, int> over_rep_map;
   std::map<std::string,int>::iterator it;
@@ -194,12 +100,12 @@ void process_fastq (std::string infile, std::string out_prefix, int buffer_size)
            qual_by_col_it = qual_by_column.find(pos_counter);
             if(qual_by_col_it != qual_by_column.end())
             {
-              qual_by_column.at(pos_counter).push_back(ascii_map.find(*it)->second);
+              qual_by_column.at(pos_counter).push_back(static_cast<int>(*it));
             }
             else
             {
               std::vector<int> tmp;
-              tmp.push_back(ascii_map.find(*it)->second);
+              tmp.push_back(static_cast<int>(*it));
               qual_by_column.insert(std::pair<int, std::vector<int> >(pos_counter,tmp));
             }
           }
@@ -287,101 +193,7 @@ std::vector<std::vector<int> > qual_score_per_position (const std::map<int,std::
 // [[Rcpp::export]]
 Rcpp::List qual_score_per_read (std::string infile)
 {
-  std::map<char, uint8_t> ascii_map;
-  ascii_map['!'] = 0;
-  ascii_map['"'] = 1;
-  ascii_map['#'] = 2;
-  ascii_map['$'] = 3;
-  ascii_map['%'] = 4;
-  ascii_map['&'] = 5;
-  ascii_map['\''] = 6;
-  ascii_map['('] = 7;
-  ascii_map[')'] = 8;
-  ascii_map['*'] = 9;
-  ascii_map['+'] = 10;
-  ascii_map[','] = 11;
-  ascii_map['-'] = 12;
-  ascii_map['.'] = 13;
-  ascii_map['/'] = 14;
-  ascii_map['0'] = 15;
-  ascii_map['1'] = 16;
-  ascii_map['2'] = 17;
-  ascii_map['3'] = 18;
-  ascii_map['4'] = 19;
-  ascii_map['5'] = 20;
-  ascii_map['6'] = 21;
-  ascii_map['7'] = 22;
-  ascii_map['8'] = 23;
-  ascii_map['9'] = 24;
-  ascii_map[':'] = 25;
-  ascii_map[';'] = 26;
-  ascii_map['<'] = 27;
-  ascii_map['='] = 28;
-  ascii_map['>'] = 29;
-  ascii_map['?'] = 30;
-  ascii_map['@'] = 31;
-  ascii_map['A'] = 32;
-  ascii_map['B'] = 33;
-  ascii_map['C'] = 34;
-  ascii_map['D'] = 35;
-  ascii_map['E'] = 36;
-  ascii_map['F'] = 37;
-  ascii_map['G'] = 38;
-  ascii_map['H'] = 39;
-  ascii_map['I'] = 40;
-  ascii_map['J'] = 41;
-  ascii_map['K'] = 42;
-  ascii_map['L'] = 43;
-  ascii_map['M'] = 44;
-  ascii_map['N'] = 45;
-  ascii_map['O'] = 46;
-  ascii_map['P'] = 47;
-  ascii_map['Q'] = 48;
-  ascii_map['R'] = 49;
-  ascii_map['S'] = 50;
-  ascii_map['T'] = 51;
-  ascii_map['U'] = 52;
-  ascii_map['V'] = 53;
-  ascii_map['W'] = 54;
-  ascii_map['X'] = 55;
-  ascii_map['Y'] = 56;
-  ascii_map['Z'] = 57;
-  ascii_map['['] = 58;
-  ascii_map['\\'] = 59;
-  ascii_map[']'] = 60;
-  ascii_map['^'] = 61;
-  ascii_map['_'] = 62;
-  ascii_map['`'] = 63;
-  ascii_map['a'] = 64;
-  ascii_map['b'] = 65;
-  ascii_map['c'] = 66;
-  ascii_map['d'] = 67;
-  ascii_map['e'] = 68;
-  ascii_map['f'] = 69;
-  ascii_map['g'] = 70;
-  ascii_map['h'] = 71;
-  ascii_map['i'] = 72;
-  ascii_map['j'] = 73;
-  ascii_map['k'] = 74;
-  ascii_map['l'] = 75;
-  ascii_map['m'] = 76;
-  ascii_map['n'] = 77;
-  ascii_map['o'] = 78;
-  ascii_map['p'] = 79;
-  ascii_map['q'] = 80;
-  ascii_map['r'] = 81;
-  ascii_map['s'] = 82;
-  ascii_map['t'] = 83;
-  ascii_map['u'] = 84;
-  ascii_map['v'] = 85;
-  ascii_map['w'] = 86;
-  ascii_map['x'] = 87;
-  ascii_map['y'] = 88;
-  ascii_map['z'] = 89;
-  ascii_map['{'] = 90;
-  ascii_map['|'] = 91;
-  ascii_map['}'] = 92;
-  ascii_map['~'] = 93;
+
 
   std::vector<double> quality_score_per_read;
   std::vector<uint8_t> qual_by_column;
@@ -403,15 +215,15 @@ Rcpp::List qual_score_per_read (std::string infile)
       int pos_counter = 1;
       for (std::string::iterator it = line.begin(); it != line.end(); ++it)
       {
-          qual_by_column.push_back(ascii_map.find(*it)->second);
+          qual_by_column.push_back(static_cast<int>(*it));
           if(pos_counter <= qual_score_matrix.size())
           {
-          qual_score_matrix[pos_counter].push_back(ascii_map.find(*it)->second);
+          qual_score_matrix[pos_counter].push_back(static_cast<int>(*it));
           }
           else
           {
             std::vector<uint8_t> tmp_qual;
-            tmp_qual.push_back(ascii_map.find(*it)->second);
+            tmp_qual.push_back(static_cast<int>(*it));
             std::pair<int,std::vector<uint8_t>> tmp = std::pair<int,std::vector<uint8_t>>(pos_counter,tmp_qual);
             qual_score_matrix.insert(tmp);
           }
